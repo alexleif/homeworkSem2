64d40ba8af7e57ce6f8775f522926256a9e1c5fd
alex <gerotyk9999@gmail.com>
2023-01-09 17:26:40 +0300

    добавил второе задание

---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число - ");
+int number = int.Parse(Console.ReadLine());
+if (number < 0) 
+{
+    number *= -1;
+}
+if (number>=100)
+{
+while (number>999)
+{
+number = number/10;
+}
+Console.WriteLine($"Третья цифра числа {number%10}");
+}
+else 
+{
+    Console.Write("Введенное число не трехначное");
+}
